9f97b54d7294aa9a9d98797361c4ea26118e93f2
jntakpe <j.ntakpe@gmail.com>
2015-06-26 23:52:33 +0200

    Initial commit

---
+++ mockrest-server/src/main/java/com/github/jntakpe/mockrest/MockrestServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.jntakpe.mockrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockrestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockrestServerApplication.class, args);
+    }
+}
